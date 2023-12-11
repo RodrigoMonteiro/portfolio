ed0f01f365177e25426c90309cd04a0336647b3b
Rodrigo Monteiro <rodrigomonteiiro17@gmail.com>
2023-12-11 15:06:03 -0300

    Creating  routes for views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './views/about-me/about-me.component';
+import { SkillsComponent } from './views/skills/skills.component';
+import { EducationComponent } from './views/education/education.component';
+import { ProjectsComponent } from './views/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"skills", component: SkillsComponent},
+  {path:"about-me", component: AboutMeComponent},
+  {path:"projects",component:ProjectsComponent},
+  {path:"education",component:EducationComponent},
+  {path: "**", component: AboutMeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common'
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BtnThemeComponent } from './components/btn-theme/btn-theme.component';
+import { RouterModule } from '@angular/router';
+
 
 
 
 
 @NgModule({
   declarations: [NavbarComponent, BtnThemeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavbarComponent, BtnThemeComponent],
 })
 export class SharedModule {}
